80e548337e0d06fcbfb186237d84fb4cfd031179
Alexey <Zimin>
2019-03-12 10:14:52 +0300

    FEAT Prepare chart && data

---
+++ src/components/ChartCanvas.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="view_wrapper">
+    hi
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChartCanvas',
+  props: {
+    dataJSON: String
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
---
+++ src/views/Chart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="view_wrapper">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chart',
+  props: {
+    dataJson: {
+      type: Array
+    }
+  },
+  created () {
+    console.log(this.dataJson)
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
